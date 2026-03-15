76bfd52e53fedd3090f16dccc1d2fd5622f32df3
Davidson <chardinpoutcheu@gmail.com>
2026-03-15 23:13:48 +0100

    Travail Complet de Davidson

---
+++ src/Controller/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\PostRepository;
+final class PageController extends AbstractController
+{
+    #[Route('/', name: 'app_home')]
+    public function index(postrepository $postRepository): Response
+    { $posts = $postRepository->findAll();
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'PageController',
+            'posts' => $posts,
+        ]);
+    }
+     #[Route('/about', name:'app_about')]
+    public function about(): Response
+    {
+        return $this->render('page/about.html.twig');
+    }
+}
---
+++ src/Controller/PostController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Entity\Comment;
+use App\Form\CommentType;
+use App\Repository\PostRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class PostController extends AbstractController
+{
+    #[Route('/post', name: 'app_post')]
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/post/{id}', name: 'app_post_show', methods: ['GET', 'POST'])]
+    public function show(Post $post, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $comment = new Comment();
+        $commentForm = $this->createForm(CommentType::class, $comment);
+        $commentForm->handleRequest($request);
+
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $comment->setPost($post);
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_post_show', ['id' => $post->getId()]);
+        }
+
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+            'commentForm' => $commentForm->createView(),
+        ]);
+    }
+}
